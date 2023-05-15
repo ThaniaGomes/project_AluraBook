c84e30537595bef54d5013cb19eaad1a04a039ba
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-05-15 13:59:59 -0300

    assincrono

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log("Mandando oi para o amigo!");
 
 function mandaMensagem(){
-
+    console.log("tudo bem?")
+    console.log("vou te mandar uma solicitação!")
+    console.log("solicitação recebida.")
 }
+
+mandaMensagem()
+
+console.log("byby")
