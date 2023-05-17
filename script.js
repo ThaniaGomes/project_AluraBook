8ce1d8e0ed0df9928b6efaf722df8575d6d8e56d
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-05-17 17:28:38 -0300

    deixando codigo mais legivel

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* 
+Opção 1:
 var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
 .then(r => {
@@ -10,3 +12,19 @@ var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
 .finally(mensagem => console.log("Processamento concluído!"))
 
 console.log(consultaCep)
+*/
+
+async function buscaCep(){
+    try{
+    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/')
+    var consultaCepJSON = await consultaCep.json()
+    if(consultaCepJSON){
+        throw Error('CEP não Existente!')
+    }
+    console.log(consultaCepJSON)
+}catch(erro){
+    console.log(erro)
+}
+}
+
+buscaCep()
