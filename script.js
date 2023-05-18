5e6e94745d439f004a7b481a54bf6e63f018e1c3
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-05-18 15:07:55 -0300

    adicionando completar automatico

--- script.js
+++ script.js
@@ -64,6 +64,17 @@ async function buscaEndereco(cep) {
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
